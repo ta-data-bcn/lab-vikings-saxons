9528e4c3f9daa3faa650e2c3549c790020db3329
tristar82 <56124104+tristar82@users.noreply.github.com>
2019-10-27 22:02:28 +0100

    need to add while true loops

--- your-code/battle/soldiers.py
+++ your-code/battle/soldiers.py
@@ -1,59 +1,59 @@
 # Soldier
 class Soldier:
-    def __init__(self, health, strength):
-        self.health = health
-        self.strength = strength
+    def __init__(self, health, strength):#, damage):
+        self.health = int(health)
+        self.strength = int(strength)
         
     def attack(self):
         return self.health
-        #return '{}'.format(self.health) #its one of these 2
       
     def receive_damage(self, damage):
+        self.damage = damage
         self.health = int(self.health - self.damage)
+        return self.health
 
 
 # Viking
 class Viking(Soldier):
-    def __init__(self, health, strength, name): #4 and 5 are new and only belong to viking
+    #### REMOVE THE BELOW ####
+    #damage = 0
+    def __init__(self, health, strength, name): #, damage): #4 and 5 are new and only belong to viking
         super().__init__(health, strength)
         self.name = name
+        #self.damage = damage
 
-    #attack to be inherited from Soldie
         
-    def receive_damage(self, damage):
-        self.health = self.health - self.damage
-        #If the Viking is still alive, the method must return "NAME has received DAMAGE points of damage".
-        #If the Viking dies, the method must return "NAME has died in combat".
-        if self.health > 0:
-            #print(f"{} has received {} points of damage" self.name self.damage)
-            #print(self.name+" has received "+self.damage+" points of damage")
-            return '{} has received {} points of damage'.format(self.name, self.damage)
-        if self.health <= 0:
-            return '{} has died in combat'.format(self.name)                
-                
+#    def receive_damage(self, damage):
+#        self.damage = damage
+#        self.health = int(self.health - self.damage)
+#
+#        if int(self.health) > 0:
+#            #print(f"{} has received {} points of damage" self.name self.damage)
+#            #print(self.name+" has received "+self.damage+" points of damage")
+#            return '{} has received {} points of damage'.format(self.name, self.damage)
+#        if int(self.health) <= 0:
+#            return '{} has died in combat'.format(self.name)                
+#                
     def battle_cry(self):
         return "Odin Owns You All!"
         
 # Saxon
 class Saxon(Soldier):
-    def __init__(self, health, strength): #4 and 5 are new and only belong to viking
-        super().__init__(health, strength)
+    
+    def __init__(self, health, strength): #, damage): #4 and 5 are new and only belong to viking
+        super().__init__(health, strength)#, damage)
 
-    #attack to be inherited from Soldie
         
-    def receive_damage(self, damage):
-        self.health = self.health - self.damage
-        #If the Viking is still alive, the method must return "NAME has received DAMAGE points of damage".
-        #If the Viking dies, the method must return "NAME has died in combat".
-        if self.health > 0:
-            #print(f"{} has received {} points of damage" self.name self.damage)
-            #print(self.name+" has received "+self.damage+" points of damage")
-            return 'A Saxon has received {} points of damage'.format(self.damage)
-        if self.health <= 0:
-            return 'A Saxon has died in combat'.format(self.name)                
+#    def receive_damage(self, damage):
+#        self.damage = damage
+#        self.health = int(self.health - self.damage)
+#        if int(self.health) > 0:
+#            return 'A Saxon has received {} points of damage'.format(self.damage)
+#        if int(self.health) <= 0:
+#            return 'A Saxon has died in combat'.format(self.name)                
                 
 
     
     
     
-    pass
+    
