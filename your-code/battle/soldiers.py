6eafa12f39c2ef4d3ae23fbd9376c786e516dd84
Jordi Jordana <jordi.jordana.1988@gmail.com>
2019-10-29 14:36:11 +0100

    Jordi whole Vikings and Saxons challenge

--- your-code/battle/soldiers.py
+++ your-code/battle/soldiers.py
@@ -1,11 +1,41 @@
 # Soldier
 class Soldier:
-    pass
+    def __init__ (self, health, strength):
+        self.health = health
+        self.strength = strength
+    
+    def attack (self):
+        return self.strength
+
+    def receive_damage(self, damage):
+        self.health = self.health - damage
 
 # Viking
 class Viking(Soldier):
-    pass
+    def __init__ (self, health, strength, name):
+        self.name = name
+        Soldier.__init__(self, health, strength)
+    
+    def receive_damage(self, damage):
+        Soldier.receive_damage(self, damage)
+        if self.health > 0:
+            return f'{self.name} has received {damage} points of damage.'
+        elif self.health == 0:
+            return f'{self.name} has died in combat.'
+    
+    def battle_cry():
+        return 'Odin owns you all!'
     
 # Saxon
 class Saxon(Soldier):
-    pass
+    def __init__ (self, health, strength):
+        Soldier.__init__(self, health, strength)
+    def receive_damage(self, damage):
+        Soldier.receive_damage(self, damage)
+        if self.health > 0:
+            return 'A Saxon has received {damage} points of damage.'
+        elif self.health == 0:
+            return 'A saxon has died in combat.'
+    
+    def battle_cry():
+        return 'Saxons are the best!'
