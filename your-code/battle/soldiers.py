6b006927ebf2dee4e2c6064e9cef098d74c818fa
IslaamSabbah <islaamsabbah@gmail.com>
2019-10-29 14:32:09 +0100

    soldiers

--- your-code/battle/soldiers.py
+++ your-code/battle/soldiers.py
@@ -1,11 +1,47 @@
 # Soldier
-class Soldier:
-    pass
+class Soldier():
+    def __init__(self,health,strength):
+        self.health = int(health)
+        self.strength = int(strength)
+    def attack(self):
+        return self.strength 
+    def recieve_damage(self,x):
+        self.health=self.health - x
 
 # Viking
 class Viking(Soldier):
-    pass
+    def __init__(self, health, strength, name):
+        Soldier.__init__(self, health, strength)
+        self.name = name
+    def strength():
+        return (int(Soldier.strength))
+        
+    def recieve_damage(self,x):
+        Soldier.recieve_damage(self,x)
+        if (int(self.health == 0)): 
+           print( self.name , "has died in combat" )
+        if (int(self.health >0)):
+            print( self.name , "has received" ,x, "of damage" )
+
+    def battle_cry():
+        print("Odin owns you all")
+    
+
     
 # Saxon
 class Saxon(Soldier):
-    pass
+    def __init__ (self,health,strength):
+        Soldier.__init__(self,health,strength)
+    
+    def recieve_damage(self,x):
+        Soldier.recieve_damage(self , x)
+        if (int(self.health == 0)):
+           print( "A Saxon has died in combat" )
+        if (int(self.health >0)):
+            print( "A Saxon has received ",x," of damage ")
+        
+
+
+new_soldier = Soldier(100,100)
+
+
