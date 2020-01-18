3e5c45b178b8b128ca93d271e59415f213d4f1bd
Rebecca Estiarte <rebeccaestiarte@gmail.com>
2020-01-18 19:05:13 +0100

    Started Lab

--- your-code/battle/soldiers.py
+++ your-code/battle/soldiers.py
@@ -1,10 +1,25 @@
 # Soldier
 class Soldier:
-    pass
-
+    def __Init__(self, strenght, health):
+        self.strenght = strenght
+        self.health = health
+        
+    def attack(self):
+        return self.strenght
+    
+    def receive_damage(self, damage):
+        return health = health - damage
+        
+    
 # Viking
 class Viking(Soldier):
-    pass
+    
+    def __Init__(self, strenght, health, name):
+        super().__init__(strenght, health) 
+        self.name = name
+    def
+    
+    
     
 # Saxon
 class Saxon(Soldier):
