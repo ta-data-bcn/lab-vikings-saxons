4a43e4146a41ab82edf62add9e88c9e508ab65ed
Kevin Forster <kevin.forster10@gmail.com>
2020-01-19 14:12:08 +0100

    lab vikings

--- your-code/battle/soldiers.py
+++ your-code/battle/soldiers.py
@@ -1,11 +1,42 @@
 # Soldier
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        
+        self.health=health
+        self.strength=strength
+    def attack(self):
+        return self.strength
+    def receive_damage(self, damage):
+        self.health -= damage
 
 # Viking
 class Viking(Soldier):
-    pass
+    def __init__(self, health, strength, name):
+        
+        self.health=health
+        self.strength=strength
+        self.name=name
+    def attack(self):
+        return self.strength
+    def receive_damage(self, damage):
+        self.health -= damage
+        if self.health>0:
+            return f"{self.name} has received {damage} points of damage."
+        else:
+            return f"{self.name} has dies in combat."
+    def battle_cry (self):
+        return "Odin Owns You All!"
     
 # Saxon
 class Saxon(Soldier):
-    pass
+    def __init__(self, health, strength):
+        self.health=health
+        self.strength=strength
+    def attack(self):
+        return self.strength
+    def receive_damage(self, damage):
+        self.health -= damage
+        if self.health>0:
+            return f"A Saxon has received {damage} points of damage."
+        else:
+            return "A Saxon has dies in combat."
