78bcf4ec62f92fd4e8aae36f6855982e5b90a95b
Rebecca Estiarte <rebeccaestiarte@gmail.com>
2020-01-20 07:55:09 +0100

    soldier.py completed, to check

--- your-code/battle/soldiers.py
+++ your-code/battle/soldiers.py
@@ -17,10 +17,37 @@ class Viking(Soldier):
     def __Init__(self, strenght, health, name):
         super().__init__(strenght, health) 
         self.name = name
-    def
     
+    
+    def attack(self): 
+        return self.strenght
+    
+    def receive_damage(self, damage, name):
+        health = health - damage
+        if health > 0:
+            print(self.name, "has received", self.damage, "points of damage")
+        else:
+            print(self.name, "has died in combat")
+        
+    def battle_cry(self):
+        print ("Odin Owns You All!")
     
     
 # Saxon
 class Saxon(Soldier):
-    pass
+    
+     def __Init__(self, strenght, health):
+        super().__init__(strenght, health)
+        self.name = name
+    
+    def attack(self): 
+        return self.strenght
+    
+    def receive_damage(self, damage):
+        health = health - damage
+        if health > 0:
+            print("A Saxon has received", self.damage, "points of damage")
+        else:
+            print("A Saxon has died in combat")
+        
+        
