76f0fba05b03d2a85cdb984b9fe571cd71a71861
Robin Langlois <langlois.robin@gmail.com>
2020-01-21 09:39:54 +0100

    commiting my Classes Viking-Saxons / not entirely working but close enough

--- your-code/battle/soldiers.py
+++ your-code/battle/soldiers.py
@@ -1,11 +1,43 @@
-# Soldier
-class Soldier:
-    pass
+#!/usr/bin/env python
+# coding: utf-8
 
-# Viking
+# In[ ]:
+
+
+#Soldier
+class Soldier:
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+
+    def attack(self):
+        return self.strength
+
+    def receive_damage(self, damage):
+        return self.health - damage
+
+#Viking
 class Viking(Soldier):
-    pass
+    def __init__(self, health, strength, name):
+        super().__init__(health, strength)
+        self.name = name
     
-# Saxon
+    def receive_damage(self, damage):
+        self.health -= damage
+        if self.health > 0:
+            return (f"{self.name} has received {damage} points of damage")
+        else:
+            return (f"{self.name} has died proudly in the combat. He went to the Valhalla")
+        
+    def battle_cry():
+        return "Odin Owns You All !"
+
+#Saxon
 class Saxon(Soldier):
-    pass
+
+    def receive_damage(self, damage):
+        self.health -= damage
+        if self.health > 0:
+            return (f"A Saxon has received {damage} points of damage")
+        else:
+            return ("A Saxon has died proudly in combat")
