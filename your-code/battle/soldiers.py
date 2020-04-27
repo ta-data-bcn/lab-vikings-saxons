abe410e96b4156fd03d7ec9e939953c7f6c57245
Francesco Baldissera <francesco.baldissera@gmail.com>
2020-04-27 19:07:39 +0200

    First edit of the OOP file

--- your-code/battle/soldiers.py
+++ your-code/battle/soldiers.py
@@ -1,10 +1,29 @@
 # Soldier
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+    
+    def attack():
+        return strength
+    
+    def receive_damage(damage):
+        return health - damage
 
 # Viking
 class Viking(Soldier):
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def battle_cry():
+        return print("Odin Owns You All!")
+
+    def receive_damage(damage):
+        health - damage
+        if Viking.health > 0:
+            return print("{name} has received {damage} points of damage")
+        else:
+            return print('{name} has died in combat!')
     
 # Saxon
 class Saxon(Soldier):
